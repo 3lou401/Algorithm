5849979ed1058c619fcd4ff281d18c38785322bf
3lou401 <1396894821@qq.com>
2019-12-20 21:50:48 +0800

    aa

--- src/com/dataStructure/TreeDefine.java
+++ src/com/dataStructure/TreeDefine.java
@@ -1,5 +1,8 @@
 package com.dataStructure;
 
+import java.util.*;
+import java.util.logging.Level;
+
 /**
  * @Author: leaderHoo
  * @Date: 2019/12/20 15:16
@@ -26,8 +29,8 @@ abstract  class Tree<T>{
 
 
 // 二叉树 ： 父亲最多拥有左右两棵子树，这里具体一下，
-// Node表示节点，BinaryTree从根节点入手（root），通过root可达任意节点
-class BinaryTree<T>{
+// Node表示节点，BTree从根节点入手（root），通过root可达任意节点
+abstract  class BTree<T>{
     private class Node<T>{
         Node<T> left,right;
         T value;
@@ -44,6 +47,250 @@ class BinaryTree<T>{
 //3. 设N0表示度为0的节点个数，N2表示度为2的节点个数， N0= N2 + 1 (叶子节点个数之和 = 有两个儿子节点个数之和+1)
 //证明 总结点数设为N,边数设为B ,则 B = N -1 ,
 // 同时B=N1+2*N2 , 得出N = N1+2*N2+1, 同时节点数N=N0+N1+N2,
+class BinaryTree<T> {
+    private class Node<T>{
+        public Node(Node left, Node right, T value) {
+            this.left = left;
+            this.right = right;
+            this.value = value;
+        }
 
+        public Node() {
+        }
+
+        Node<T> left, right; T value; }
+
+        Node<T> root;
+
+      /**
+       * 根据层序数组生成二叉树
+        * @param [datas] 是按照层序组织的树的所有数据 ； 利用队列
+        * @desc        // 1. 先取出一个树，作为根，入队列
+       *             // 2. 创建一个队列 ； 只要队列不为空就循环，循环体内，弹出当前节点（要对其增加左右儿子），
+       *             按照（左右）顺序，数组中有数据，新建一个节点入队列
+       */
+        public Node<T> createTreeByLayerSequen(T[] datas ){
+            if (datas.length == 0)
+                return null;
+            Queue<Node<T>> q  = new LinkedList<>();
+            Node<T> root = new Node<>(null,null,datas[0]);
+            q.offer(root);
+            int count = 1;
+            // 如果数据没了，队列中还有节点，无所谓，因为初始化时，已经指定了左右节点默认为空，无需显示处理一下
+            while (!q.isEmpty() && count < datas.length){
+               Node<T> tempRoot = q.poll();
+                //还有数据
+                 if (count < datas.length){
+                    T val = datas[count];
+                    tempRoot.left  = new Node<>(null,null,val);
+                    count++;
+                    q.offer(tempRoot.left );
+                }else {
+                     tempRoot.left = null;
+                 }
+                 //还有数据
+                 if (count < datas.length){
+                    T val = datas[count];
+                    tempRoot.right  = new Node<>(null,null,val);
+                    count++;
+                    q.offer(tempRoot.right );
+                }else{
+                     tempRoot.right = null;
+                 }
+            }
+            return root;
+        }
+
+        /**
+         *先序生成二叉树
+         * @createTime 2019/12/20 20:20
+         * @param
+         * @desc 数据是输入的, 判断输入数据
+         */
+        public   Node<Integer> create(Node<Integer> node) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("请输入节点的值：");
+            int value = in.nextInt();
+
+            if (value != 0) {
+                node = new Node();
+                node.value = value;
+                node.left = create(node.left);
+                node.right = create(node.right);
+            } else {
+                //下面的步骤至关重要！！！
+                node = null;
+            }
+            return node;
+        }
+        /**
+         * 求二叉树的高度: 递归求
+         * @param [node]
+         * @return int
+         */
+        public int height(Node<T> node){
+            if (node == null){
+                return 0;
+            }
+            int leftH = height(node.left);
+            int rightH = height(node.right);
+            return Math.max(leftH,rightH) + 1;
+        }
+        /**
+         *打印叶子节点
+         * @param [node]
+         * @desc  采用先序遍历方式，不同的是，加上了条件
+         */
+        public void printleafNode(Node<T> node){
+            if (node == null)
+                return;
+             if (node.left == null && node.right ==null)
+                 System.out.printf("--"+node.value);
+             printleafNode(node.left);
+             printleafNode(node.right);
+         }
+         /**
+          * 层序遍历
+          * @param [root]
+          * @desc
+          */
+         public void layerTraversal(Node<T> root){
+             if (root == null)
+                 return;
+             Queue<Node<T>> myq = new LinkedList<>();
+             myq.offer(root);
+             while (!myq.isEmpty()){
+                 Node<T> temp = myq.poll();
+                 System.out.printf(""+temp.value);
+                 if (temp.left!= null)
+                     myq.offer(temp.left);
+                 if (temp.right!= null)
+                     myq.offer(temp.right);
+             }
+         }
+         // 三种递归遍历
+        /**
+         * 先序编列
+         * @author leaderHoo
+         * @createTime 2019/12/20 21:02
+         */
+         public void preOrderTraversal(Node<T> bt){
+             if (bt != null){
+                 System.out.printf("-"+bt.value);
+                 preOrderTraversal(bt.left);
+                 preOrderTraversal(bt.right);
+             }
+         }
+        /**
+         * 中序编列
+         * @author leaderHoo
+         * @createTime 2019/12/20 21:02
+         */
+         public void midOrderTraversal(Node<T> bt){
+             if (bt != null){
+                 midOrderTraversal(bt.left);
+                 System.out.printf("-"+bt.value);
+                 midOrderTraversal(bt.right);
+             }
+         }
+        /**
+         * 后序编列
+         * @author leaderHoo
+         * @createTime 2019/12/20 21:02
+         */
+         public void backOrderTraversal(Node<T> bt){
+             if (bt != null){
+                 backOrderTraversal(bt.left);
+                 System.out.printf("-"+bt.value);
+                 backOrderTraversal(bt.right);
+             }
+         }
+         //三种非递归遍历
+        /**
+         * 非递归先序遍历
+         * @author leaderHoo
+         * @createTime 2019/12/20 21:06
+         */
+        public void preOrderTraversalNoResc(Node<T> bt){
+            if (bt ==null)
+                return;
+            Stack<Node<T>> s = new Stack<>();
+            while (!s.empty() && bt != null){
+                //先序 ： 根左右 ， 思路 ：打印（就是处理根节点），入栈，指向左儿子
+                while (bt != null){
+                    System.out.printf("-"+bt.value); //先序遍历，入栈之前先访问
+                    s.push(bt);
+                    bt = bt.left;
+                }
+                Node<T> temp = s.pop();
+                bt = temp.right;
+            }
+
+        }
+        /**
+         * 中序遍历 左根右
+         * @author leaderHoo
+         * @desc 思路 只要左儿子不空，全入栈 ；
+         *         一直到左儿子空了，弹出（此节点无左儿子，可能有右儿子），打印此节点根， 指向右儿子，处理；
+         *
+         *
+         *    先序和中序类似 ： 不同之处，先序是入栈的时候，就要访问节点（从根节点看顺序，根左右）
+         *     中序是 到最左边的节点（该节点左儿子为空），再访问这个节点、右儿子
+         */
+        public void midOrderTraversalNoResc(Node<T> bt){
+            Stack<Node<T>> s = new Stack<>();
+            while (bt != null && !s.isEmpty()){
+                while (bt != null){
+                    s.push(bt);
+                    bt = bt.left;
+                }
+                Node<T> temp = s.pop();
+                System.out.printf("--"+temp.value); // 中序遍历： 弹出之后，再访问
+                bt = temp.right;
+            }
+
+        }
+
+
+    public void postTraByStack() {
+        System.out.println("后序非递归遍历，借助栈");
+        //新建栈，先进后出,将根结点入栈,双端队列
+        Deque<Node> stack = new LinkedList<>();
+        //新建一个list，记录结点的状态是否已经被访问过
+        ArrayList<Node> list = new ArrayList<>();
+//		stack.push(root);
+        Node proot;
+        Node node = root;
+        int flag;
+        //首先检查完树的左子树，再右子树，最后将根节点输出
+        while (node != null || stack.size() > 0) {
+            //将最左子树添加完毕
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            //和中序遍历相似，为先输出左结点，但是做结点输出完毕之后，不能直接将根结点弹出，而是必须先将右结点弹出，
+            //最后再将根结点弹出来，就会牵扯到一个根结点的访问状态的问题，是否已经被遍历过了
+            //利用一个list集合记录已将被遍历过的根结点，防止产生死循环
+            if (stack.size() > 0) {
+                Node peek = stack.peek();
+                if (peek.right != null) {
+                    boolean con = list.contains(peek);
+                    if (con == true) {
+                        Node pop = stack.pop();
+                        System.out.print(pop.value + "  ");
+                    } else {
+                        list.add(peek);
+                        node = peek.right;
+                    }
+                } else {
+                    Node pop = stack.pop();
+                    System.out.print(pop.value + "  ");
+                }
+            }
+        }
+    }
+
+}
 
 
